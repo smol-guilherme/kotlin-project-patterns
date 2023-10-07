e90c6c7533d908b8b9f566f22b9bd1cdd4f820ee
Guilherme de Oliveira - Giggs <smo.guilherme93@gmail.com>
2023-10-07 09:06:41 -0300

    first commit

---
+++ src/main/kotlin/dio/ssf_application/SsfApplication.kt
@@ -0,0 +1,13 @@
+package dio.ssf_application
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
+
+@EnableFeignClients
+@SpringBootApplication
+class SsfApplication
+
+fun main(args: Array<String>) {
+	runApplication<SsfApplication>(*args)
+}
---
+++ src/test/kotlin/dio/ssf_application/SsfApplicationTests.kt
@@ -0,0 +1,13 @@
+package dio.ssf_application
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SsfApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
