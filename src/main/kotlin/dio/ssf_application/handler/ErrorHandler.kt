c84a44e23048a957a67dc8ede560658a3c80c503
Guilherme de Oliveira - Giggs <smo.guilherme93@gmail.com>
2023-10-09 17:08:32 -0300

    fix: added a handler for external api calls.

--- src/main/kotlin/dio/ssf_application/handler/ErrorHandler.kt
+++ src/main/kotlin/dio/ssf_application/handler/ErrorHandler.kt
@@ -1,6 +1,7 @@
 package dio.ssf_application.handler
 
 import dio.ssf_application.handler.errors.InsufficientInformationException
+import feign.FeignException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -19,5 +20,11 @@ class ErrorHandler {
   fun <T: Throwable> handleInsufficientInformation(ex: T): ResponseEntity<String> {
     val msg = "Error: ${ex.message} is too short/incomplete"
     return ResponseEntity(msg, HttpStatus.UNPROCESSABLE_ENTITY)
+  }
+
+  @ExceptionHandler(FeignException::class)
+  fun <T: Throwable> handleInvalidPostalCode(ex: T): ResponseEntity<String> {
+    val msg = "Error: Postal Code (CEP) is not valid"
+    return ResponseEntity(msg, HttpStatus.BAD_REQUEST)
   }
 }
